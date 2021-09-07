ca5a89722111ff0a6a7b26524a9d21a2496c5ddf
Dan <dan@daniel-lewis.com>
2021-09-07 19:31:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,131 +1,26 @@
-document.getElementById("69").onclick = function(){ // ask button clicked
-
-var NumberChosen = Math.floor(Math.random() * 19); // picks random number
-    
-    if(NumberChosen == 0){ // AFFIRMATIVE
-
-            document.getElementById("420").innerHTML = "It is Certain.";
-
-    }
-
-    else if(NumberChosen == 1){
-
-            document.getElementById("420").innerHTML = "It is decidedly so.";
-
-    }
-
-    else if(NumberChosen == 2){
-
-            document.getElementById("420").innerHTML = "Without a doubt.";
-
-    }
-
-    else if(NumberChosen == 3){
-
-            document.getElementById("420").innerHTML = "Yes definitely.";
-
-    }
-
-    else if(NumberChosen == 4){
-
-            document.getElementById("420").innerHTML = "You may rely on it.";
-
-    }
-
-    else if(NumberChosen == 5){
-
-            document.getElementById("420").innerHTML = "As I see it, yes.";
-
-    }
-
-    else if(NumberChosen == 6){
-
-            document.getElementById("420").innerHTML = "Most likely.";
-
-    }
-
-    else if(NumberChosen == 7){
-
-            document.getElementById("420").innerHTML = "Outlook good.";
-
-    }
-
-    else if(NumberChosen == 8){
-
-            document.getElementById("420").innerHTML = "Yes.";
-
-    }
-
-    else if(NumberChosen == 9){
-
-            document.getElementById("420").innerHTML = "Signs point to yes.";
-
-    }
-
-    else if(NumberChosen == 10){ // Non-committal
-
-            document.getElementById("420").innerHTML = "Reply hazy, try again.";
-
-    }
-
-    else if(NumberChosen == 11){
-
-            document.getElementById("420").innerHTML = "Ask again later.";
-
-    }
-
-    else if(NumberChosen == 12){
-
-            document.getElementById("420").innerHTML = "Better not tell you now.";
-
-    }
-
-    else if(NumberChosen == 13){
-
-            document.getElementById("420").innerHTML = "Cannot predict now.";
-
-    }
-
-    else if(NumberChosen == 14){
-
-            document.getElementById("420").innerHTML = "Concentrate and ask again.";
-
-    }
-
-    else if(NumberChosen == 15){ // Negative
-
-            document.getElementById("420").innerHTML = "Don't count on it.";
-
-    }
-
-    else if(NumberChosen == 16){
-
-            document.getElementById("420").innerHTML = "My sources say no.";
-
-    }
-
-    else if(NumberChosen == 17){
-
-            document.getElementById("420").innerHTML = "Outlook not so good.";
-
-    }
-
-    else if(NumberChosen == 18){
-
-            document.getElementById("420").innerHTML = "Very doubtful.";
-
-    }
-
-    else if(NumberChosen == 19){
-
-            document.getElementById("420").innerHTML = "My reply is no.";
-
-    }
-
-    else if(NumberChosen == 20){
-
-            document.getElementById("420").innerHTML = "My sources say no.";
-
-    }
-
-}
+const responses = ["It is Certain.", 
+                  "It is decidedly so.", 
+                  "Without a doubt.", 
+                  "Yes definitely.", 
+                  "You may rely on it.", 
+                  "As I see it, yes.",
+                  "Most likely",
+                  "Outlook good.",
+                  "Yes.",
+                  "Signs point to yes.",
+                  "Reply hazy, try again.",
+                  "Ask again later.",
+                  "Better not tell you now.",
+                  "Cannot predict now.",
+                  "Concentrate and ask again.",
+                  "Don't count on it.",
+                  "My sources say no.",
+                  "Outlook not so good.",
+                  "Very doubtful.",
+                  "My reply is no.",
+                  "My sources say no."];
+
+document.getElementById("69").addEventListener("click", () => {
+  var randomChoice = Math.floor(Math.random() * responses.length);
+  document.getElementById("420").innerText = responses[randomChoice];
+});
