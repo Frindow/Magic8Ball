d1a3179e2ebc2851a5b3006da9eab911185b3ae2
Frindow <69374353+NotFrindow@users.noreply.github.com>
2021-09-08 21:29:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-const responses = ["It is Certain.", 
-                  "It is decidedly so.", 
-                  "Without a doubt.", 
-                  "Yes definitely.", 
-                  "You may rely on it.", 
-                  "As I see it, yes.",
-                  "Most likely",
-                  "Outlook good.",
-                  "Yes.",
-                  "Signs point to yes.",
-                  "Reply hazy, try again.",
-                  "Ask again later.",
-                  "Better not tell you now.",
-                  "Cannot predict now.",
-                  "Concentrate and ask again.",
-                  "Don't count on it.",
-                  "My sources say no.",
-                  "Outlook not so good.",
-                  "Very doubtful.",
-                  "My reply is no.",
-                  "My sources say no."];
+const responses = ["🟢It is Certain.", 
+                  "🟢It is decidedly so.", 
+                  "🟢Without a doubt.", 
+                  "🟢Yes definitely.", 
+                  "🟢You may rely on it.", 
+                  "🟢As I see it, yes.",
+                  "🟢Most likely",
+                  "🟢Outlook good.",
+                  "🟢Yes.",
+                  "🟡Signs point to yes.",
+                  "🟡Reply hazy, try again.",
+                  "🟡Ask again later.",
+                  "🟡Better not tell you now.",
+                  "🟡Cannot predict now.",
+                  "🟡Concentrate and ask again.",
+                  "🔴Don't count on it.",
+                  "🔴My sources say no.",
+                  "🔴Outlook not so good.",
+                  "🔴Very doubtful.",
+                  "🔴My reply is no.",
+                  "🔴My sources say no.",];
 
 document.getElementById("69").addEventListener("click", () => {
+
   var randomChoice = Math.floor(Math.random() * responses.length);
   document.getElementById("420").innerText = responses[randomChoice];
+});
+document.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+    var randomChoice = Math.floor(Math.random() * responses.length);
+    document.getElementById("420").innerText = responses[randomChoice];
+    }
 });
